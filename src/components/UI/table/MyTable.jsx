319eeb86e2cc843fd33335e3b37bd7074fc924ae
avcshka <forceregeneration@gmail.com>
2022-08-08 17:52:28 +0300

    feat: add report table

--- src/components/UI/table/MyTable.jsx
+++ src/components/UI/table/MyTable.jsx
@@ -1,66 +1,29 @@
 import React from 'react';
 import classes from './MyTable.module.css'
 
-const MyTable = () => {
-
+const MyTable = ({rows, columns}) => {
     return (
-        <div>
-            <table className={classes.customers}>
-                <tr>
-                    <th>Company</th>
-                    <th>Contact</th>
-                    <th>Country</th>
-                </tr>
-                <tr>
-                    <td>Alfreds Futterkiste</td>
-                    <td>Maria Anders</td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td>Berglunds snabbköp</td>
-                    <td>Christina Berglund</td>
-                    <td>Sweden</td>
-                </tr>
-                <tr>
-                    <td>Centro comercial Moctezuma</td>
-                    <td>Francisco Chang</td>
-                    <td>Mexico</td>
-                </tr>
-                <tr>
-                    <td>Ernst Handel</td>
-                    <td>Roland Mendel</td>
-                    <td>Austria</td>
-                </tr>
-                <tr>
-                    <td>Island Trading</td>
-                    <td>Helen Bennett</td>
-                    <td>UK</td>
-                </tr>
-                <tr>
-                    <td>Königlich Essen</td>
-                    <td>Philip Cramer</td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td>Laughing Bacchus Winecellars</td>
-                    <td>Yoshi Tannamuri</td>
-                    <td>Canada</td>
-                </tr>
-                <tr>
-                    <td>Magazzini Alimentari Riuniti</td>
-                    <td>Giovanni Rovelli</td>
-                    <td>Italy</td>
-                </tr>
-                <tr>
-                    <td>North/South</td>
-                    <td>Simon Crowther</td>
-                    <td>UK</td>
-                </tr>
-                <tr>
-                    <td>Paris spécialités</td>
-                    <td>Marie Bertrand</td>
-                    <td>France</td>
-                </tr>
+        <div className={classes.myTable}>
+            <table>
+                {columns.map(column => {
+                    return (
+                        <th>
+                            <tr>{column}</tr>
+                        </th>
+                    )
+                })}
+
+                {rows.map((row, key) => {
+                    return (
+                        <tr key={key}>
+                            {columns.map(column => {
+                                return (
+                                    <td>{row[column]}</td>
+                                )
+                            })}
+                        </tr>
+                    )
+                })}
             </table>
         </div>
     );
--- src/components/mainview/MyMainView.jsx
+++ src/components/mainview/MyMainView.jsx
@@ -1,41 +1,31 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from "./MyMainView.module.css"
 import MyTable from "../UI/table/MyTable";
 import MyHeaderView from "./headerview/MyHeaderView";
 import axios from "axios";
 
 const MyMainView = () => {
+    const [rows, setRows] = useState([])
+    const [columns, setColumns] = useState([])
 
-    // const [table, setTable] = useState([])
-    //
-    // async function fetchTable() {
-    //     const responseTable =  await axios.get('https://test.oculeus.com/supportreport/get_report?dbeg=2022-05-31&dend=2022-08-03&ttt=2&typ=1')
-    // }
+    useEffect(() => {
+        fetchTable()
+    }, [])
 
-
-    const [test, setTest] = useState([])
-    const info = [
-        {"USER NAME": "Irina Grosu", "SCORE OCULEUS": 1640, "EFFICIENCY OCULEUS": 12.62,},
-        {"USER NAME": "Igor Visotski","SCORE OCULEUS": 1085,"EFFICIENCY OCULEUS": 6.46},
-        {"USER NAME": "Natalia Bucicovscaia","SCORE OCULEUS": 680,"EFFICIENCY OCULEUS": 5.27},
-        {"USER NAME": "Vlada Androsova","SCORE OCULEUS": 925,"EFFICIENCY OCULEUS": 5.26},
-        {"USER NAME": "Michael Sumarev","SCORE OCULEUS": 870,"EFFICIENCY OCULEUS": 5.12},
-        {"USER NAME": "Svetozar A","SCORE OCULEUS": 70,"EFFICIENCY OCULEUS": 0.40}
-    ]
-
+    async function fetchTable() {
+        const responseTable =  await axios.get('https://test.oculeus.com/supportreport/get_report?dbeg=2022-05-31&dend=2022-08-03&ttt=2&typ=1')
+        setRows(responseTable.data);
+        setColumns(Object.keys(responseTable.data[0]));
+    }
 
     return (
         <div className={classes.myMainView}>
 
-            <MyHeaderView>
-
-            </MyHeaderView>
+            <MyHeaderView/>
 
             <hr/>
 
-            <MyTable>
-
-            </MyTable>
+            <MyTable columns={columns} rows={rows}/>
 
         </div>
     );
